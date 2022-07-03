52a35b0664fe759e6fb79254c4d98ddddf4cd89b
Saul <saul.navarro.utj@gmail.com>
2022-07-03 01:02:33 -0500

    Dependencies installed

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const routerProducts = require('./src/router/products.router');
+
 const app = express();
+const PORT = process.env.PORT || 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+app.use('/products', routerProducts);
 
+const server = app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});
+
+server.on('error', (error) => console.error(error));
